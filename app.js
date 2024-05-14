ff267a12a24b68860b02b2e62efa905da89a7752
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-14 20:34:19 +0530

    Async code, Boiling function runs in the background while we chop the carrots

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 // let's boilWater
 
+/* A) synchronous water boiling ---
 console.log('Getting ready to boil the water')
 
 function boilWater() {
@@ -13,3 +14,18 @@ function boilWater() {
 boilWater()
 
 console.log('Boiling for water is done')
+*/
+
+// B) Asynchronous water boiling and chopping carrots to make soup
+
+console.log('Start Chopping carrots')
+boilWater(10000)
+
+function boilWater(time) {
+  console.log('boiling ...')
+  setTimeout(time => {
+    console.log('Boiling Done')
+  }, time)
+}
+
+console.log('Keep chopping carrots...')
